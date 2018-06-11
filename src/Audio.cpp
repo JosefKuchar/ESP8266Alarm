dde44e57c27b9a08c8e98208c1f5f7d42846c8b0
Josef Kuchař <josef.kuchar267@gmail.com>
2018-06-11 18:27:10 +0200

    Audio implementation

---
+++ src/Audio.cpp
@@ -0,0 +1,35 @@
+#include "Audio.h"
+
+void Audio::init() {
+    out = new AudioOutputI2SNoDAC();
+    mp3 = new AudioGeneratorMP3(); 
+}
+
+bool Audio::isRunning() {
+    return mp3->isRunning();
+}
+
+void Audio::loop() {
+    if (!mp3->loop()) {
+        currentFile++;
+
+        mp3->stop();
+        id3->close();
+        file->close();
+
+        if (currentFile >= fileCount) {
+            return;
+        }
+
+        file = new AudioFileSourceSPIFFS(("/" + String(currentFile) + ".mp3").c_str());
+        id3 = new AudioFileSourceID3(file);
+        mp3->begin(id3, out);
+    }
+}
+
+void Audio::play(int len) {
+    file = new AudioFileSourceSPIFFS("/0.mp3");
+    id3 = new AudioFileSourceID3(file);
+    mp3->begin(id3, out);
+    fileCount = len;
+}
--- src/Audio.h
+++ src/Audio.h
@@ -2,14 +2,28 @@
 #define Audio_h
 
 #include "Settings.h"
+#include <ESP8266Spiram.h>
+#include <AudioFileSourceSPIFFS.h>
+#include <AudioGeneratorMP3.h>
+#include <AudioOutputI2SNoDAC.h>
+#include <AudioFileSourceID3.h>
 
 extern Settings settings;
 
 class Audio {
     public:
         void init();
+        bool isRunning();
+        void loop();
+        void play(int len);
     private:
-        
-}
+        AudioGeneratorMP3 *mp3;
+        AudioFileSourceSPIFFS *file;
+        AudioOutputI2SNoDAC *out;
+        AudioFileSourceID3 *id3;
+        int currentFile;
+        int fileCount;
+        char* fileNames[];
+};
 
 #endif
